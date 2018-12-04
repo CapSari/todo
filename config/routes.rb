45a0e92c78cc62913eef9e496135eade116ce82c
CapSari <dvesari@gmail.com>
2018-12-04 16:02:32 +0300

    added ability to close on todo list items once done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
   end
   root "todo_lists#index"
   # See how all your routes lay out with "rake routes".
